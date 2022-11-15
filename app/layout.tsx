3516ba341c36f69096d7726e86f3c7ec6233076d
Jacob <jacobryanstevenson@gmail.com>
2022-11-15 15:02:29 -0600

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @next/next/no-head-element */
+
+import Link from 'next/link';
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html>
+            <body>
+                <nav>
+                    <Link href="/">Home</Link>
+                    <Link href="/notes">Notes</Link>
+                </nav>
+                {children}
+            </body>
+        </html>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>World</h1>
+            <p>Hello</p>
+        </div>
+    );
+}
